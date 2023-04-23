a4e71b086b0a370c3518c6c888b261843a69f2f5
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-04-23 00:58:22 -0400

    updated router with userMangaList search and malUsernameSearch functions

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-// import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import type { AxiosError } from "axios";
 import { contextProps } from "@trpc/react-query/shared";
 import { api } from "~/utils/api";
 import type { PrismaClient, User } from "@prisma/client";
+import { Token } from "@clerk/nextjs/dist/api";
+import { env } from "process";
 
 //publicProcedure method to generate a function that your client calls
 //publicProcedure is a procedure that anyone can call without being authenticated, cause we want anyone to have access to the post
@@ -12,21 +15,64 @@ type Context = {
   prisma: PrismaClient;
 };
 
-const malUsernameSearch = (username: string) => {
-  console.log(username);
-  const axios = require("axios");
-};
-const isUserNull = async (user: User[], username: string, ctx: Context) => {
-  if (Array.isArray(user) && user.length === 0) {
-    await ctx.prisma.user.create({
-      data: {
-        username: username,
-      },
-    });
+const userMangaListSearch = (username: string) => {
+  console.log(`username is ${username}`);
+  const tokenInfo = process.env.NEXT_PUBLIC_MAL_API_ACCESS_TOKEN;
+  const headers = {
+    Authorization: `Bearer ${tokenInfo}`,
+  };
 
+  const params = {
+    fields: "list_status",
+    limit: "1000",
+    // 'offset': '5'
+  };
+  axios
+    .get(`https://api.myanimelist.net/v2/users/${username}/mangalist`, {
+      headers: headers,
+      params: params,
+    })
+    .then((response) => {
+      for (const manga of response.data.data) {
+        console.log(manga);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const malUsernameSearch = async (username: string): Promise<string> => {
+  try {
+    await axios.get(`https://myanimelist.net/profile/${username}`);
+    return "success";
+  } catch (error) {
+    const err = error as AxiosError;
+    if (err.response?.status === 404) {
+      return "failure";
+    }
+    throw error;
+  }
+};
+
+const isUserNull = async (user: User[], username: string, ctx: Context) => {
+  console.log(user[0]?.username);
+  if (Object.keys(user).length === 0) {
+    try {
+      const queryResponse = await malUsernameSearch(username);
+      if (queryResponse === "success")
+        await ctx.prisma.user.create({
+          data: {
+            username: username,
+          },
+        });
+      return userMangaListSearch(username);
+    } catch (error) {
+      return "failure";
+    }
+  } else {
     return "success";
   }
-  return user;
 };
 
 export const userRouter = createTRPCRouter({
