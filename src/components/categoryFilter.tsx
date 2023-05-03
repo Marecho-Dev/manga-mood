16ed478c6b895bd4adca094f7fb21e58d10d77bd
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-05-02 23:45:52 -0400

    prettier

--- src/components/categoryFilter.tsx
+++ src/components/categoryFilter.tsx
@@ -12,61 +12,71 @@
   }
   ```
 */
-import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Menu, Popover, Transition } from '@headlessui/react'
-import { XMarkIcon } from '@heroicons/react/24/outline'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Fragment, useState } from "react";
+import {
+  Dialog,
+  Disclosure,
+  Menu,
+  Popover,
+  Transition,
+} from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 const filters = [
-
   {
-    id: 'status',
-    name: 'status',
+    id: "status",
+    name: "status",
     options: [
-      { value: 'finished', label: 'finished' },
-      { value: 'on hiatus', label: 'on hiatus' },
-      { value: 'publishing', label: "publishing" },
+      { value: "finished", label: "finished" },
+      { value: "on hiatus", label: "on hiatus" },
+      { value: "publishing", label: "publishing" },
     ],
   },
   {
-    id: 'genres',
-    name: 'genres',
+    id: "genres",
+    name: "genres",
     options: [
-      { value: 'Action', label: 'Action' },
-      { value: 'Comedy', label: 'Comedy' },
-      { value: 'Drama', label: 'Drama' },
+      { value: "Action", label: "Action" },
+      { value: "Comedy", label: "Comedy" },
+      { value: "Drama", label: "Drama" },
     ],
   },
-]
-
+];
 
 type onApplyFilters = (filters: filter) => void;
-type filter = ({genres: string[],status: string[]});
+type filter = { genres: string[]; status: string[] };
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
-export const CategoryFilter = ({onApplyFilters}: {onApplyFilters: onApplyFilters}) => {
-  const [open, setOpen] = useState(false)
-  const onFilterChange = (e:React.FormEvent<HTMLFormElement>) => {
+export const CategoryFilter = ({
+  onApplyFilters,
+}: {
+  onApplyFilters: onApplyFilters;
+}) => {
+  const [open, setOpen] = useState(false);
+  const onFilterChange = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const selectedFiltersArray: filter = {genres: [], status: [] };
+    const selectedFiltersArray: filter = { genres: [], status: [] };
     filters.forEach((section) => {
-        section.options.forEach((option, optionIdx) => {
-          const inputElement = e.currentTarget.querySelector(`#filter-${section.id}-${optionIdx}`) ;
-          const isChecked = inputElement instanceof HTMLInputElement && inputElement.checked;
-          if (isChecked) {
-            if (section.id === "genres"){
+      section.options.forEach((option, optionIdx) => {
+        const inputElement = e.currentTarget.querySelector(
+          `#filter-${section.id}-${optionIdx}`
+        );
+        const isChecked =
+          inputElement instanceof HTMLInputElement && inputElement.checked;
+        if (isChecked) {
+          if (section.id === "genres") {
             selectedFiltersArray.genres.push(option.value);
-        }else if (section.id === "status"){
+          } else if (section.id === "status") {
             selectedFiltersArray.status.push(option.value);
-        }
           }
-        });
-        
-    })
-    onApplyFilters(selectedFiltersArray)    
-    };
+        }
+      });
+    });
+    onApplyFilters(selectedFiltersArray);
+  };
 
   return (
     <div className="bg-gray-900">
@@ -97,7 +107,9 @@ export const CategoryFilter = ({onApplyFilters}: {onApplyFilters: onApplyFilters
             >
               <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
                 <div className="flex items-center justify-between px-4">
-                  <h2 className="text-lg font-medium text-slate-100">Filters</h2>
+                  <h2 className="text-lg font-medium text-slate-100">
+                    Filters
+                  </h2>
                   <button
                     type="button"
                     className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-100 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500"
@@ -111,15 +123,24 @@ export const CategoryFilter = ({onApplyFilters}: {onApplyFilters: onApplyFilters
                 {/* Filters */}
                 <form className="mt-4">
                   {filters.map((section) => (
-                    <Disclosure as="div" key={section.name} className="border-t border-gray-200 px-4 py-6">
+                    <Disclosure
+                      as="div"
+                      key={section.name}
+                      className="border-t border-gray-200 px-4 py-6"
+                    >
                       {({ open }) => (
                         <>
                           <h3 className="-mx-2 -my-3 flow-root">
                             <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-sm text-gray-400">
-                              <span className="font-medium text-gray-900">{section.name}</span>
+                              <span className="font-medium text-gray-900">
+                                {section.name}
+                              </span>
                               <span className="ml-6 flex items-center">
                                 <ChevronDownIcon
-                                  className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-5 w-5 transform')}
+                                  className={classNames(
+                                    open ? "-rotate-180" : "rotate-0",
+                                    "h-5 w-5 transform"
+                                  )}
                                   aria-hidden="true"
                                 />
                               </span>
@@ -128,7 +149,10 @@ export const CategoryFilter = ({onApplyFilters}: {onApplyFilters: onApplyFilters
                           <Disclosure.Panel className="pt-6">
                             <div className="space-y-6">
                               {section.options.map((option, optionIdx) => (
-                                <div key={option.value} className="flex items-center">
+                                <div
+                                  key={option.value}
+                                  className="flex items-center"
+                                >
                                   <input
                                     id={`filter-mobile-${section.id}-${optionIdx}`}
                                     name={`${section.id}[]`}
@@ -156,94 +180,93 @@ export const CategoryFilter = ({onApplyFilters}: {onApplyFilters: onApplyFilters
           </div>
         </Dialog>
       </Transition.Root>
-        <section aria-labelledby="filter-heading" className=" py-6">
-          <h2 id="filter-heading" className="sr-only">
-            Manga Filters
-          </h2>
+      <section aria-labelledby="filter-heading" className=" py-6">
+        <h2 id="filter-heading" className="sr-only">
+          Manga Filters
+        </h2>
 
-          <div className="flex items-center justify-between">
-            <Menu as="div" className="relative inline-block text-left">
+        <div className="flex items-center justify-between">
+          <Menu as="div" className="relative inline-block text-left">
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            ></Transition>
+          </Menu>
 
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
+          <button
+            type="button"
+            className="inline-block text-sm font-medium text-slate-100 hover:text-slate-200 sm:hidden"
+            onClick={() => setOpen(true)}
+          >
+            Filters
+          </button>
+
+          <Popover.Group className="hidden sm:flex sm:items-baseline sm:space-x-8">
+            {filters.map((section, sectionIdx) => (
+              <Popover
+                as="div"
+                key={section.name}
+                id={`desktop-menu-${sectionIdx}`}
+                className="relative inline-block text-left"
               >
-              </Transition>
-            </Menu>
+                <div>
+                  <Popover.Button className="group inline-flex items-center justify-center text-sm font-medium text-slate-100 hover:text-slate-200">
+                    <span>{section.name}</span>
 
-            <button
-              type="button"
-              className="inline-block text-sm font-medium text-slate-100 hover:text-slate-200 sm:hidden"
-              onClick={() => setOpen(true)}
-            >
-              Filters
-            </button>
-                                    
-            <Popover.Group className="hidden sm:flex sm:items-baseline sm:space-x-8">
-              {filters.map((section, sectionIdx) => (
-                <Popover
-                  as="div"
-                  key={section.name}
-                  id={`desktop-menu-${sectionIdx}`}
-                  className="relative inline-block text-left"
+                    <ChevronDownIcon
+                      className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                      aria-hidden="true"
+                    />
+                  </Popover.Button>
+                </div>
+
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
                 >
-                  <div>
-                    <Popover.Button className="group inline-flex items-center justify-center text-sm font-medium text-slate-100 hover:text-slate-200">
-                      <span>{section.name}</span>
-
-                      <ChevronDownIcon
-                        className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-                        aria-hidden="true"
-                      />
-                    </Popover.Button>
-                  </div>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Popover.Panel className="absolute right-0 z-20 mt-2 origin-top-right rounded-md bg-white p-4 shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <form className="space-y-4" onSubmit={onFilterChange}>
-                        {section.options.map((option, optionIdx) => (
-                          <div key={option.value} className="flex items-center">
-                            <input
-                              id={`filter-${section.id}-${optionIdx}`}
-                              name={`${section.id}[]`}
-                              defaultValue={option.value}
-                              type="checkbox"
-                              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                            />
-                            <label
-                              htmlFor={`filter-${section.id}-${optionIdx}`}
-                              className="ml-3 whitespace-nowrap pr-6 text-sm font-medium text-gray-900"
-                            >
-                              {option.label}
-                            </label>
-                            
-                          </div>
-                        ))}
-                            <button
-                            type="submit"
-                            className="mt-4 px-4 py-2 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-500"
-                            >Apply</button>
-                      </form>
-                    </Popover.Panel>
-                  </Transition>
-                </Popover>
-              ))}
-            </Popover.Group>
-          </div>
-        </section>
-      </div>
-  )
-}
+                  <Popover.Panel className="absolute right-0 z-20 mt-2 origin-top-right rounded-md bg-white p-4 shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <form className="space-y-4" onSubmit={onFilterChange}>
+                      {section.options.map((option, optionIdx) => (
+                        <div key={option.value} className="flex items-center">
+                          <input
+                            id={`filter-${section.id}-${optionIdx}`}
+                            name={`${section.id}[]`}
+                            defaultValue={option.value}
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                          />
+                          <label
+                            htmlFor={`filter-${section.id}-${optionIdx}`}
+                            className="ml-3 whitespace-nowrap pr-6 text-sm font-medium text-gray-900"
+                          >
+                            {option.label}
+                          </label>
+                        </div>
+                      ))}
+                      <button
+                        type="submit"
+                        className="mt-4 rounded bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-indigo-500"
+                      >
+                        Apply
+                      </button>
+                    </form>
+                  </Popover.Panel>
+                </Transition>
+              </Popover>
+            ))}
+          </Popover.Group>
+        </div>
+      </section>
+    </div>
+  );
+};
