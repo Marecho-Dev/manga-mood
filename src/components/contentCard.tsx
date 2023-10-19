655c437a841b5b682fe8694420a0cb52db4db85a
Marek Staszkiewicz <Marekstaszkiewicz@live.com>
2023-10-19 10:42:20 -0400

    added tooltip for truncated titles

--- src/components/contentCard.tsx
+++ src/components/contentCard.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import { AiOutlineLink } from "react-icons/ai";
+import { useFloating, useInteractions } from "@floating-ui/react";
+import { useState } from "react";
+import { useHover } from "@floating-ui/react";
 //this is the home page
 
 type MangaData = {
@@ -19,6 +22,15 @@ type MangaData = {
 };
 
 export const ContentCard = (mangaData: MangaData) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+  });
+
+  const hover = useHover(context);
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover]);
+
   const isEmpty = (text: string): string => {
     if (text == "") {
       return "N/A";
@@ -37,12 +49,12 @@ export const ContentCard = (mangaData: MangaData) => {
     <div className="relative">
       <div className="flex h-full w-full">
         {/* Squares */}
-        <div className="absolute top-0 right-0 mt-4 mr-2 space-y-1 text-right font-bold text-gray-600">
+        <div className="absolute right-0 top-0 mr-2 mt-4 space-y-1 text-right font-bold text-gray-600">
           <div className="h-5 w-10">#{mangaData.rank}</div>
           <div className="h-5 w-10">{mangaData.rating}</div>
         </div>
         {/* //image container */}
-        <div className="relative h-full w-2/5 flex-none overflow-hidden rounded-t bg-cover text-center lg:rounded-t-none lg:rounded-l">
+        <div className="relative h-full w-2/5 flex-none overflow-hidden rounded-t bg-cover text-center lg:rounded-l lg:rounded-t-none">
           <Image
             src={mangaData.imageUrl}
             alt={mangaData.title}
@@ -64,19 +76,40 @@ export const ContentCard = (mangaData: MangaData) => {
           </div>
         </div>
         {/* content container */}
-        <div className="flex h-full w-3/5 flex-col justify-between rounded-b border-r border-b border-l border-gray-400 bg-white p-4 leading-normal lg:rounded-b-none lg:rounded-r lg:border-l-0 lg:border-t lg:border-gray-400">
+        <div className="flex h-full w-3/5 flex-col justify-between rounded-b border-b border-l border-r border-gray-400 bg-white p-4 leading-normal lg:rounded-b-none lg:rounded-r lg:border-l-0 lg:border-t lg:border-gray-400">
           <div className="mb-8">
             <p className="flex items-center text-sm text-gray-600">
               {isEmpty(mangaData.media_type)}
               <p> &nbsp;·&nbsp; </p>
               <div>{isEmpty(mangaData.status)}</div>
             </p>
-            <div className="text-l mb-2 pr-10 font-bold text-gray-900">
-              {mangaData.title}
+            <div className="text-l group relative mb-2 pr-10 font-bold text-gray-900">
+              <p
+                className="truncate"
+                ref={refs.setReference}
+                {...getReferenceProps()}
+              >
+                {mangaData.title}
+              </p>
+              {isOpen && (
+                <div
+                  ref={refs.setFloating}
+                  style={{
+                    ...floatingStyles,
+                    backgroundColor: "rgba(0, 0, 0, 1)",
+                    padding: "10px",
+                    zIndex: 9999,
+                  }}
+                  {...getReferenceProps()}
+                  className="overflow-visible rounded-md bg-slate-800 text-xs text-slate-50"
+                >
+                  {mangaData.title}
+                </div>
+              )}
             </div>
             <div className="group relative h-56">
               <p
-                className="h-56 overflow-hidden pr-2 pl-1 text-xs text-gray-700 group-hover:h-56 group-hover:overflow-x-visible group-hover:overflow-y-scroll group-hover:pr-1 group-hover:pl-1"
+                className="h-56 overflow-hidden pl-1 pr-2 text-xs text-gray-700 group-hover:h-56 group-hover:overflow-x-visible group-hover:overflow-y-scroll group-hover:pl-1 group-hover:pr-1"
                 style={{
                   position: "absolute",
                   width: "100%",
@@ -94,7 +127,7 @@ export const ContentCard = (mangaData: MangaData) => {
             {genreArray.map((genre: string) => (
               <span
                 key={genre}
-                className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-xs font-semibold text-gray-700"
+                className="mb-2 mr-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-xs font-semibold text-gray-700"
               >
                 {genre}
               </span>
